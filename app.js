9ffed997d79c0fb32137887f8219c5f9f3028a85
molarmanful <benjinpang@gmail.com>
2022-11-10 17:28:28 -0500

    little change

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ server.get('/ws', { websocket: true }, (conn, req) => {
 //   })
 // })
 
-server.listen({ port: 8080 }, (err, addr) => {
+let port = process.env.PORT || 8080
+server.listen({ port }, (err, addr) => {
   if (err) {
     server.log.error(err)
     process.exit(1)
