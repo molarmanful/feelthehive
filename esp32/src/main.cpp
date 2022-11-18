e85ad045049615c197bdde5478e1bd962f982c44
Benjamin Pang <molarmanful@users.noreply.github.com>
2022-11-18 13:02:40 -0500

    hmmm

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -4,10 +4,6 @@ MyServo servos[] = {12, 13, 14, 15, 18, 19};
 
 void setup() {
   Serial.begin(115200);
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3);
   for (auto& servo : servos) {
     servo.init();
     Serial.println(servo.pin);
