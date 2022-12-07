273208b8ce5fb3f3735fa8dbbad92698cae6705a
Benjamin Pang <benjinpang@gmail.com>
2022-12-07 02:26:18 -0500

    hmmm

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -146,8 +146,9 @@ void connSock() {
 void loop0(void* params) {
   while (1) {
     if (busy) {
-      // int p1 = min(3, 5 - max(0, vpow - 50) / 10);
-      int p1 = 5;
+      vpow = min(vpow, 91);
+      // int p1 = 5;
+      int p1 = max(3, 5 - max(0, vpow - 50) / 10);
       // int p2 = min(5, max(1, vpow / 10));
       int p2 = min(3, max(1, vpow / 10));
       int p3 = 180 - max(0, vpow - 50) * 9 / 5;
